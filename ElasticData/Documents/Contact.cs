df5a33e08a4399463e2afc63b6693e85164296bd
sv.volkoff@gmail.com <sv.volkoff@gmail.com>
2019-12-04 00:22:15 +0400

    Add NEST Elastic Client

---
+++ ElasticData/Documents/Contact.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElasticData.Documents
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
