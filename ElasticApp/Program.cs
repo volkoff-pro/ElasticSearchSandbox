881cfd88eeb80351c2f6b7aba803a978fd26e2d2
sv.volkoff@gmail.com <sv.volkoff@gmail.com>
2019-12-03 23:54:29 +0400

    Init

---
+++ ElasticApp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ElasticApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
